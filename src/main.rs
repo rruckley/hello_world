383a72ec47aacebf3aac7056a365b59511a92513
Ryan <rruckley@gmail.com>
2023-04-02 19:00:17 +1000

    Added Arc+Mutex

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
 mod data;
 use data::Data;
+use std::thread;
+use std::sync::Mutex;
+use std::sync::Arc;
+
 fn main() {
-    let mut d = Data::new();
-    d.add_term("Test".to_string());
-    dbg!(d);
+    let a = Arc::new(Mutex::new(Data::new()));
+    let a1 = a.clone();
+    let a2 = a.clone();
+    let t1 = thread::spawn(move || {
+        let _ = a1.lock().unwrap().add_term("term".to_string());
+        });
+    let t2 = thread::spawn(move || {
+        let _ = a2.lock().unwrap().add_term("another term".to_string());
+    });
+
+    t1.join();
+    t2.join();
+
+    let t3 = thread::spawn(move || {
+        a.lock().unwrap().print_data();
+    });
+    t3.join();
+
+    
+    
 }
