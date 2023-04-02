8490cb8920bc932a0afbaa7dc3f8742477e7a48c
Ryan <rruckley@gmail.com>
2023-04-02 19:01:44 +1000

    add print_data

--- src/data.rs
+++ src/data.rs
@@ -10,12 +10,23 @@ impl Data {
     pub fn new() -> Self {
         Self {
             qty : 0,
-            terms: vec![]
+            terms: vec![],
         }
     }
 
     pub fn add_term(&mut self, term : String) -> Result<(),String> {
         self.terms.push(term);
         Ok(())
+    }
+
+    pub fn print_data(&self) {
+        println!("Qty: {}",&self.qty);
+        self.print_terms();
+    }
+
+    pub fn print_terms(&self) {
+        for term in self.terms.iter() {
+            println!("Term: {}",term);
+        }
     }
 }
